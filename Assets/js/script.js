c2a23236d7f3b98bf5498c59c7f5735500aaef70
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-05-25 21:15:31 -0400

    md y requerimientos

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+/*Requerimientos */
+/*
+1.- La aplicación debe tener una interfaz que permita al usuario ingresar un mensaje y seleccionar un destinatario.
+2.- La aplicación debe utilizar Promesas para manejar la lógica del envío de mensajes.
+3.- Cuando el usuario envíe un mensaje, la Promesa debe cumplirse y mostrar una notificación indicando que el mensaje ha sido enviado.
+4.- Después de que se envíe un mensaje, la Promesa debe rechazarse si el usuario intenta enviar otro mensaje y mostrar una advertencia que indique que no se puede enviar otro mensaje.
+5.- La Promesa debe manejar correctamente la situación en la que un usuario intente enviar múltiples mensajes, cumpliendo con la regla de permitir solo un mensaje a la vez.
+6.- La interfaz de usuario debe proporcionar una forma clara para que el usuario ingrese un mensaje y seleccione un destinatario.
+7.- La aplicación debe tener un comportamiento predecible y consistente en términos de cómo maneja las Promesas y muestra las notificaciones y advertencias.
+8.- La aplicación debe funcionar correctamente y cumplir con todos los requisitos establecidos.*/
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DRILLING-WALKIE-TALKIE-CON-PROMESAS
+
+Walkie-Talkie con Promesas
+Esta es una aplicación en línea que simula el funcionamiento de un walkie-talkie. Permite a los usuarios enviar mensajes a un destinatario específico utilizando Promesas en JavaScript.
+
+Requisitos
+Antes de comenzar con este proyecto, asegúrate de haber revisado la lectura y los videos relacionados con las Promesas y Async/Await.
+
+Funcionalidad
+La aplicación consta de una interfaz que permite al usuario ingresar un mensaje y seleccionar un destinatario. A continuación se describen las reglas específicas implementadas en la lógica de la aplicación:
+
+Un usuario solo puede enviar un mensaje.
+Después de enviar un mensaje, se muestra una notificación indicando que el mensaje ha sido enviado.
+Si un usuario intenta enviar otro mensaje, se muestra una advertencia que indica que no se puede enviar otro mensaje.
