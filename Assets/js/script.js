c370771ddda426bba9ee47dea8a11d350d14aeee
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-05-26 19:10:32 -0400

    terminado

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,9 +9,66 @@
 7.- La aplicación debe tener un comportamiento predecible y consistente en términos de cómo maneja las Promesas y muestra las notificaciones y advertencias.
 8.- La aplicación debe funcionar correctamente y cumplir con todos los requisitos establecidos.*/
 
-//Constantes y variables
+// Constantes y variables
 const mensaje = document.getElementById("mensaje");
 const destinatario = document.getElementById("destinatario");
-const btnEnviar = document.getElementById("btnEnviar");
+const btnEnviar = document.querySelector("form button");
 const success = document.getElementById("verde");
 const wrong = document.getElementById("rojo");
+let isSending = false; // Variable para rastrear si se está enviando un mensaje
+
+// Función para mostrar la notificación de éxito
+const mostrarExito = (mensaje) => {
+  success.innerHTML = mensaje;
+  success.classList.add("show");
+  setTimeout(() => {
+    success.classList.remove("show");
+  }, 3000);
+};
+
+// Función para mostrar la notificación de error
+const mostrarError = (mensaje) => {
+  wrong.innerHTML = mensaje;
+  wrong.classList.add("show");
+  setTimeout(() => {
+    wrong.classList.remove("show");
+  }, 3000);
+};
+
+// Función para enviar el mensaje
+const enviarMensaje = () => {
+  return new Promise((resolve, reject) => {
+    if (isSending) {
+      reject("Ya has enviado un mensaje. No puedes enviar otro.");
+    } else if (mensaje.value === "" || destinatario.value === "") {
+      reject("No se puede enviar el mensaje");
+    } else {
+      isSending = true; // Marcar como enviando mensaje
+
+      // Simular el envío del mensaje (aquí puedes agregar la lógica de envío real)
+      setTimeout(() => {
+        resolve("Mensaje enviado");
+      }, 2000);
+    }
+  });
+};
+
+// Evento al enviar el formulario
+btnEnviar.addEventListener("click", (event) => {
+  event.preventDefault(); // Evitar el envío del formulario
+
+  enviarMensaje()
+    .then((res) => {
+      mostrarExito(res);
+
+      // Restablecer los campos y el estado de envío después de 3 segundos
+      setTimeout(() => {
+        mensaje.value = "";
+        destinatario.value = "";
+        isSending = false;
+      }, 3000);
+    })
+    .catch((err) => {
+      mostrarError(err);
+    });
+});
